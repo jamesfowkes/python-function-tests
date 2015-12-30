f5ed7ce19a20a613d019e56d2605878a2f11b3f6
James Fowkes <jamesfowkes@gmail.com>
2015-12-30 19:18:12 +0000

    Strip punctuation tests pass

--- 04_strip_punctuation.py
+++ 04_strip_punctuation.py
@@ -3,17 +3,67 @@ import unittest
 ###################################
 #### Write your function below! ####
 import string
-def strip_punctuation(string_in):
-    string_out = ""
-    for i in string_in:
+def find_index_of_first_nonpunctation_in_string(word):
+    for pos, i in enumerate(word):
         if i not in string.punctuation:
-            string_out = string_out + i
-    return string_out
+            return pos
+
+def find_index_of_last_nonpunctation_in_string(word):
+    flip_word = word[::-1]
+    for pos, i in enumerate(flip_word):
+        if i not in string.punctuation:
+            return (len(word) - pos) - 1
+
+def strip_punctuation(string_in):
+    start_pos = find_index_of_first_nonpunctation_in_string(string_in)
+    end_pos = find_index_of_last_nonpunctation_in_string(string_in)
+    return string_in[start_pos:end_pos+1]
+
+
 ###################################
 ###################################
 
 
 class FunctionTest(unittest.TestCase):
+
+    def test_find_index_of_first_nonpunctation_in_string(self):
+        expected = None
+        actual = find_index_of_first_nonpunctation_in_string(",")
+        self.assertEqual(expected, actual)
+
+        expected = 0
+        actual = find_index_of_first_nonpunctation_in_string("word")
+        self.assertEqual(expected, actual)
+
+        expected = 1
+        actual = find_index_of_first_nonpunctation_in_string(",word")
+        self.assertEqual(expected, actual)
+        
+        expected = 6
+        actual = find_index_of_first_nonpunctation_in_string(".!.[],didn't")
+        self.assertEqual(expected, actual)
+
+    def test_find_index_of_last_nonpunctation_in_string(self):
+        expected = None
+        actual = find_index_of_last_nonpunctation_in_string(",")
+        self.assertEqual(expected, actual)
+
+        expected = 3
+        actual = find_index_of_last_nonpunctation_in_string("word")
+        self.assertEqual(expected, actual)
+
+        expected = 4
+        actual = find_index_of_last_nonpunctation_in_string(",word")
+        self.assertEqual(expected, actual)
+            
+        expected = 11
+        actual = find_index_of_last_nonpunctation_in_string(".!.[],didn't")
+        self.assertEqual(expected, actual)
+
+    def test_strip_punctuation_leaves_midword_punctation_intact(self):
+        expected = "didn't"
+        actual = strip_punctuation("didn't")
+        self.assertEqual(expected, actual)
 
     def test_strip_punctuation_leaves_standard_word_intact(self):
         expected = "word"
@@ -67,10 +117,6 @@ class FunctionTest(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
-    def test_strip_punctuation_leaves_midword_punctation_intact(self):
-        expected = "didn't"
-        actual = strip_punctuation("didn't")
-        self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
     unittest.main()
